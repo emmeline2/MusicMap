f1f2e0a5637d923c8f70707eb1df44d8f0771d83
emmeline <emmeline.pearson@case.edu>
2022-08-23 15:21:53 -0400

    fetching posts

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlinte3.row_factory
+    conn.row_factory = sqlite3.Row
     return conn
 
 app = Flask(__name__)
@@ -13,4 +13,4 @@ def index():
     conn = get_db_connection()
     posts = conn.execute('SELECT * FROM posts').fetchall()
     conn.close()
-    return render_template('index.html')
+    return render_template('index.html', posts=posts)
