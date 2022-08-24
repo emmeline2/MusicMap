9f9b3420440b657561be01ced37b51e393d8481f
emmeline <emmeline.pearson@case.edu>
2022-08-24 13:31:56 -0400

    working map and songs list

---
+++ init_db.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+connection = sqlite3.connect('database.db')
+
+with open('schema.sql') as f:
+    connection.executescript(f.read())
+
+cur = connection.cursor()
+
+cur.execute("INSERT INTO posts (title, content) values (?,?)",
+           ('First Song', 'Artist: A -- Content for the first Song')
+           )
+
+cur.execute("INSERT INTO posts (title, content) values (?,?)",
+           ('Second Song', 'Artist: B -- Content for the second song')
+           )
+
+connection.commit()
+connection.close()
--- static/js/universal.js
+++ static/js/universal.js
@@ -1,5 +1,5 @@
 var map = L.map('map').fitWorld();
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
